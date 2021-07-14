75c4c056cd32608bad6433fef3104ad9ec01bed8
iqbalgithub1998 <iqbalkhanak45@gmail.com>
2021-07-14 12:08:01 +0530

    Email Problem Fixed

--- services/mailService.js
+++ services/mailService.js
@@ -1,13 +1,13 @@
 const nodemailer = require("nodemailer");
 module.exports = async ({ from, to, subject, text, html}) => {
         let transporter = nodemailer.createTransport({
-            host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
-            secure: false, // true for 465, false for other ports
-            auth: {
-                user: process.env.MAIL_USER, // generated ethereal user
-                pass: process.env.MAIL_PASSWORD, // generated ethereal password
-            },
+          host: process.env.SMTP_HOST,
+          port: process.env.SMTP_PORT,
+          secure: false, // true for 465, false for other ports
+          auth: {
+            user: process.env.MAIL_USER, // generated ethereal user
+            pass: process.env.MAIL_PASS, // generated ethereal password
+          },
         });
 
         // send mail with defined transport object
